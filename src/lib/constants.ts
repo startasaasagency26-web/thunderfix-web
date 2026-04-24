28c4d065bee69b6b95b74fb59d9453d7f6432f65
startasaasagency26-web <startasaasagency26@gmail.com>
2026-04-24 19:15:44 +0800

    feat: add Google Reviews and Facebook social media constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,3 @@
-export const GOOGLE_REVIEWS_URL = "https://share.google/W4kiym7rddhhE0Cli";
+export const GOOGLE_REVIEWS_URL =
+  "https://www.google.com/search?sca_esv=aad7451a74ba4d56&hl=en-MY&sxsrf=ANbL-n6_lNTR-HJXyHzsUQONB9Wz6J9Pjw:1777027133245&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOTqBMD2YG_e157yreaVG7raYb529EhZppMtIEOp_KANvhuybhT9GAdwyIj1DwAzYb9jmbimBClYtabpVe4cj4w1XSpW6Z_AM1TFaWVerthCD9KqamQ%3D%3D&q=Thunderfix+Seri+Kembangan+Reviews&sa=X&ved=2ahUKEwjq26H4pYaUAxUNzjgGHQLGPVkQ0bkNegQIIhAF&cshid=1777027234899225&biw=1536&bih=695&dpr=1.25";
 export const FACEBOOK_URL = "https://www.facebook.com/share/18Q78Fk8vS/?mibextid=wwXIfr";
